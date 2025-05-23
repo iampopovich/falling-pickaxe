2a575ea24d3cb7389f4f7bdc2d545760d240a529
Victor Sandu <vyctor661@gmail.com>
2025-05-23 20:09:04 +0300

    feat: config.json gets automatically copied from default.config.json if it doesnt exist

--- src/config.py
+++ src/config.py
@@ -1,6 +1,12 @@
 import json
+import os
+import shutil
+
+# If config.json does not exist, copy from default.config.json
+if not os.path.exists("config.json"):
+    print("config.json not found. Copying default.config.json to config.json.")
+    shutil.copy("default.config.json", "config.json")
 
 # Load configuration from config.json
 with open("config.json", "r") as config_file:
     config = json.load(config_file)
-
